eddf2fd306aeef0fc4206a0f0345d3092fd5c388
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-03-13 09:55:23 +0800

    Update nscript.js

--- nscript.js
+++ nscript.js
@@ -129,4 +129,11 @@ const searchbaractive=()=>{
         });
     }
 } 
+window.onscroll = function() {myFunction()};
 
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progress-bar").style.width = scrolled + "%";
+}
